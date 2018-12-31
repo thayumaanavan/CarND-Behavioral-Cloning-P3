9d22caaa6f1a746287ba6784ea58e3e21631cf2d
Thayumaanavan C R <thayumaanavan@hotmail.com>
2018-12-31 12:38:46 +0000

    cleanup

--- model.py
+++ model.py
@@ -1,100 +1,93 @@
 import csv
-import cv2
 import numpy as np
-from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
-from scipy import ndimage
+import cv2
 import sklearn
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from keras.layers import Cropping2D, Conv2D, MaxPooling2D
+from sklearn.utils import shuffle
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Conv2D, Lambda, Dropout
+from keras.layers.convolutional import Cropping2D
+from keras.optimizers import Adam
 
-
-#load data
+# Read and store lines from driving data log
 samples = []
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    next(reader) # skips header line
     for line in reader:
         samples.append(line)
-
-# Read images from the training
-def read_image(path):
-    image = cv2.imread(path)
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # drive.py loads images in rgb
-    return image_rgb
-
-def generator_images(samples, batch_size = 32):
+        
+# Create a data Generator        
+def generator(samples, batch_size=32):
     num_samples = len(samples)
-    while True:
-        sklearn.utils.shuffle(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
+            
+            # Create empty arrays to hold images and steering values
             images = []
-            measurements = []
-            steering_correction = 0.2
+            angles = []
+            
             # For each line in the driving data log, read camera image (left, right and centre) and steering value
             for batch_sample in batch_samples:
-                image_centre = read_image('data/'+ '/IMG/' + line[0].split('/')[-1])
-                image_left = read_image('data/'+ '/IMG/' + line[1].split('/')[-1])
-                image_right = read_image('data/'+ '/IMG/' + line[2].split('/')[-1])
-                steering_centre = float(batch_sample[3])
-                steering_left = steering_centre + steering_correction
-                steering_right = steering_centre - steering_correction
-                images.extend([image_centre, image_left, image_right])
-                measurements.extend([steering_centre, steering_left, steering_right])
+                for i in range(3): # center, left and rights images
+                    name = 'data/IMG/' + batch_sample[i].split('/')[-1]
+                    current_image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
+                    images.append(current_image)
+                    # Correction value for steering angle for left and right camera images        
+                    steering_correction = 0.2
+                    center_angle = float(batch_sample[3])
+                    if i == 0:
+                        angles.append(center_angle)
+                    elif i == 1: 
+                        angles.append(center_angle + steering_correction2)
+                    elif i == 2: 
+                        angles.append(center_angle - steering_correction)
+                    
+                    images.append(cv2.flip(current_image, 1))
+                    
+                    # Augment training data by flipping images and changing sign of steering
+                    if i == 0:
+                        angles.append(center_angle * -1.0)
+                    elif i == 1: 
+                        angles.append((center_angle + steering_correction) * -1.0)
+                    elif i == 2: 
+                        angles.append((center_angle - steering_correction) * -1.0)
             
-            # Augment training data by flipping images and changing sign of steering
-            augmented_images, augmented_measurements = [], []
-            for image,measurement in zip(images, measurements):
-                augmented_images.append(image)
-                augmented_measurements.append(measurement)
-                augmented_images.append(cv2.flip(image,1))
-                augmented_measurements.append(measurement*-1.0) 
-                
-            X_train = np.array(augmented_images)
-            Y_train = np.array(augmented_measurements)
-            yield sklearn.utils.shuffle(X_train, Y_train)
+            # Convert images and steering_angles to numpy arrays for Keras to accept as input  
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
 # split driving data to train and validate
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_samples, validation_samples = train_test_split(samples[1:], test_size=0.2)
 
 # Use generator to pull data 
-batch_size=32
-train_generator = generator_images(train_samples, batch_size)
-validation_generator = generator_images(validation_samples, batch_size)
+m_batch_size = 32
+train_generator = generator(train_samples, batch_size=m_batch_size)
+validation_generator = generator(validation_samples, batch_size=m_batch_size)
 
-#NVIDIA Architecture
+# nVidia model
 model = Sequential()
-model.add(Lambda(lambda x : x/255.0 - 0.5, input_shape = (160,320,3)))
-model.add(Cropping2D(cropping = ((70,25),(0,0))))
-model.add(Conv2D(filters= 24, kernel_size = 5, strides = 2, activation='relu'))
-model.add(Conv2D(filters= 36, kernel_size = 5, strides = 2, activation='relu'))
-model.add(Conv2D(filters= 48, kernel_size = 5, strides = 2, activation='relu'))
-model.add(Conv2D(filters= 64, kernel_size = 3, strides = 1, activation='relu'))
-model.add(Conv2D(filters= 64, kernel_size = 3, strides = 1, activation='relu'))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Conv2D(24,(5,5), strides=(2,2), activation='relu'))
+model.add(Conv2D(36,(5,5), strides=(2,2), activation='relu'))
+model.add(Conv2D(48,(5,5), strides=(2,2), activation='relu'))
+model.add(Conv2D(64,(3,3), activation='relu'))
+model.add(Conv2D(64,(3,3), activation='relu'))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
+model.add(Dense(1164, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
 
-print(model.summary())
+# fit the model
+model.fit_generator(train_generator, steps_per_epoch=len(train_samples)//m_batch_size,
+validation_data=validation_generator, validation_steps=len(validation_samples)//m_batch_size, epochs=5, verbose = 1)
 
-model.compile(loss = 'mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch = int(len(train_samples)/batch_size),
-                    validation_data = validation_generator, nb_val_samples = int(len(validation_samples)/batch_size),
-                    nb_epoch = 3)
+# Save model
 model.save('model.h5')
-
-### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
-
-
-        
